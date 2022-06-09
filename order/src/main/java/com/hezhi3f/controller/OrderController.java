045a10b25c53165886bf095dbc2e095733c175bf
hezhi3f <hezhi3f@foxmail.com>
2022-06-09 21:14:48 +0800

    改了OrderController的uri，此外新版SpringCloud已经不支持Ribbon，无论如何都报错

--- order/src/main/java/com/hezhi3f/controller/OrderController.java
+++ order/src/main/java/com/hezhi3f/controller/OrderController.java
@@ -2,6 +2,7 @@ package com.hezhi3f.controller;
 
 import com.hezhi3f.entity.Payment;
 import com.hezhi3f.result.Result;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
@@ -14,15 +15,24 @@ public class OrderController {
     @Resource
     private RestTemplate restTemplate;
 
-    @PostMapping("/payment/save")
+    @PostMapping("/save")
     public Result save(Payment payment) {
         return restTemplate.postForObject(PAYMENT_URL + "/payment/save", payment, Result.class);
     }
 
-    @GetMapping("/payment/get/{id}")
+    @GetMapping("/get/{id}")
     public Result getPaymentById(@PathVariable("id") Integer id) {
         return restTemplate.getForObject(PAYMENT_URL + "/payment/get/" + id, Result.class);
     }
 
+    @GetMapping("/getEntity/{id}")
+    public Result getPaymentEntityById(@PathVariable("id") Integer id) {
+        ResponseEntity<Result> entity = restTemplate.getForEntity(PAYMENT_URL + "/payment/get/" + id, Result.class);
+        if (entity.getStatusCode().is2xxSuccessful()) {
+            return entity.getBody();
+        } else {
+            return new Result(400, "查询错误", null);
+        }
+    }
 
 }
